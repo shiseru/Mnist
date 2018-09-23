df5b9d56609cc2bde9c24aa1dfe7c9f8b27a74a5
Shisei Naka <nathaniel.naka@gmail.com>
2018-09-23 13:39:49 +0900

    set the train and test part

--- torch_mnist.py
+++ torch_mnist.py
@@ -64,7 +64,43 @@ model = Net().to(device)
 LEARNING_RATE = 0.01
 MOMENTUM = 0.5
 
-optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 
+LOG_INTERVAL = 10
 
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % LOG_INTERVAL == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data),
+                                                                           len(train_loader.dataset),
+                                                                           100. * batch_idx / len(train_loader), loss.item()))
+
+def test():
+    model.eval() #swith to guessing mode
+    test_loss = 0
+    correct = 0
+    with torch.no_grad(): #Do not learn to change parameters
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+EPOCHS = 3 # 3 epoch is good enough to reach accuracy 97%
+for epoch in range(1, EPOCHS + 1):
+    train(epoch)
+    test()
 
